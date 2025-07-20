1312efcb6acfd83a305521e8f0f702de97122e3f
Renato Yoiti <renatoyoiti@gmail.com>
2025-07-20 10:07:25 -0300

    Fontes personalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts
+} from '@expo-google-fonts/host-grotesk';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import './src/styles/global.css';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  let [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+  
   return (
     <View className="bg-lime-500 flex-1 items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className="text-base">Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
