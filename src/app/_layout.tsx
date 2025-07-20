7be97cff96ae79586d49ee8e717b912cd83c891b
Renato Yoiti <renatoyoiti@gmail.com>
2025-07-20 14:59:28 -0300

    Protected routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -30,9 +30,20 @@ export default function RootLayout() {
   if (!loaded && !error) {
     return null;
   }
+
+  const isLoggedIn = false;
+
   return (
     <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }}/>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen name="(private)" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen name="(public)" />
+        </Stack.Protected>
+      </Stack>
     </SafeAreaProvider>
   );
 }
