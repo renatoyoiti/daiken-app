edd943edd48aacd5b555b9cadc7a710a57241433
Renato Yoiti <renatoyoiti@gmail.com>
2025-07-20 09:53:28 -0300

    Initial Structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import './src/styles/global.css';
+
+export default function App() {
+  return (
+    <View className="bg-lime-500 flex-1 items-center justify-center">
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
